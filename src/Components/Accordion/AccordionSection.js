5a814b2492fc3245f150745661df8a1c7b9ca230
Mir Sujat <mir.sujat@gmail.com>
2021-11-30 15:34:08 +0600

    Update AccordionSection.js

--- src/Components/Accordion/AccordionSection.js
+++ src/Components/Accordion/AccordionSection.js
@@ -2,10 +2,6 @@ import React, { Component, Fragment } from "react";
 
 
 class AccordionSection extends Component {
-    constructor(props) {
-        super(props);
-         
-      }
 
 
   onClick = () => {
