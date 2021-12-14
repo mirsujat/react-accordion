d05c4028955ac2354631f9385aa11ea49a056295
Mir Sujat <mir.sujat@gmail.com>
2021-12-14 20:28:01 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,6 +37,32 @@ function App() {
               </AccordionSection>
         </Accordion>
         </section>
+         <section className="content">
+          <h2>multiSelect  open multiple section open at a time</h2>
+          <Accordion multiSelect>
+              <AccordionSection label="Section 1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet.
+                </p>
+              </AccordionSection>
+              <AccordionSection label="Section 2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet.
+                </p>
+              </AccordionSection>
+              <AccordionSection label="Section 3">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet.
+                </p>
+              </AccordionSection>
+        </Accordion>
+        </section>
       </main>
       <Footer></Footer>
     </div>
